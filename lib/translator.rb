03078a45b7a3f59431e50f8fd04217f8619afba1
Maharaj Syed <maharajsyed@yahoo.com>
2020-03-06 17:35:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,11 +21,11 @@ end
 
 def get_english_meaning
   def get_english_meaning(file_path = './lib/emoticons.yml',japanese_emoticon)
-  library = load_library(file_path)
-  if library["get_meaning"].include?(japanese_emoticon) 
-    library["get_meaning"][japanese_emoticon]
-  else
-    "Sorry, that emoticon was not found"
+    library = load_library(file_path)
+    if library["get_meaning"].include?(japanese_emoticon) 
+          library["get_meaning"][japanese_emoticon]
+      else
+          "Sorry, that emoticon was not found"
+      end
+    end
   end
-end
-end
