6c2e904cf992b9bbde2204c59395eb27bf1d664f
Maharaj Syed <maharajsyed@yahoo.com>
2020-03-06 17:31:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ end
 
 def get_english_meaning
   def get_english_meaning(file_path = './lib/emoticons.yml',japanese_emoticon)
-  # code goes here	  library = load_library(file_path)
+  library = load_library(file_path)
   if library["get_meaning"].include?(japanese_emoticon) 
     library["get_meaning"][japanese_emoticon]
   else
